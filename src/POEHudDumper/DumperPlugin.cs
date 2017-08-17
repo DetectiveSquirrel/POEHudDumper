6478081cbe00d3d6274fb085c43382a3e70628b9
Detective Squirrel <ratman7539@windowslive.com>
2017-08-17 23:20:35 +1200

    Added more known file extensions
    Split up extension list as it was too far down the screen and unreadable

--- src/POEHudDumper/DumperPlugin.cs
+++ src/POEHudDumper/DumperPlugin.cs
@@ -75,12 +75,21 @@ namespace POEHudDumper
 
         private bool CheckExtension(string text)
         {
+            if (text.Contains(".act") && !Settings.act) { return false; }
+            if (text.Contains(".ais") && !Settings.ais) { return false; }
             if (text.Contains(".amd") && !Settings.amd) { return false; }
             if (text.Contains(".ao") && !Settings.ao) { return false; }
+            if (text.Contains(".aoc") && !Settings.aoc) { return false; }
             if (text.Contains(".arm") && !Settings.arm) { return false; }
             if (text.Contains(".ast") && !Settings.ast) { return false; }
+            if (text.Contains(".atlas") && !Settings.atlas) { return false; }
             if (text.Contains(".bank") && !Settings.bank) { return false; }
+            if (text.Contains(".bat") && !Settings.bat) { return false; }
+            if (text.Contains(".cfg") && !Settings.cfg) { return false; }
+            if (text.Contains(".cht") && !Settings.cht) { return false; }
+            if (text.Contains(".clt") && !Settings.clt) { return false; }
             if (text.Contains(".dat") && !Settings.dat) { return false; }
+            if (text.Contains(".dct") && !Settings.dct) { return false; }
             if (text.Contains(".dds") && !Settings.dds) { return false; }
             if (text.Contains(".ddt") && !Settings.ddt) { return false; }
             if (text.Contains(".dgr") && !Settings.dgr) { return false; }
@@ -89,23 +98,39 @@ namespace POEHudDumper
             if (text.Contains(".env") && !Settings.env) { return false; }
             if (text.Contains(".epk") && !Settings.epk) { return false; }
             if (text.Contains(".et") && !Settings.et) { return false; }
+            if (text.Contains(".ffx") && !Settings.ffx) { return false; }
             if (text.Contains(".fmt") && !Settings.fmt) { return false; }
+            if (text.Contains(".fx") && !Settings.fx) { return false; }
             if (text.Contains(".gft") && !Settings.gft) { return false; }
             if (text.Contains(".gt") && !Settings.gt) { return false; }
+            if (text.Contains(".idl") && !Settings.idl) { return false; }
+            if (text.Contains(".idt") && !Settings.idt) { return false; }
+            if (text.Contains(".jpeg") && !Settings.jpeg) { return false; }
+            if (text.Contains(".jpg") && !Settings.jpg) { return false; }
             if (text.Contains(".mat") && !Settings.mat) { return false; }
+            if (text.Contains(".mb") && !Settings.mb) { return false; }
+            if (text.Contains(".mel") && !Settings.mel) { return false; }
             if (text.Contains(".mtd") && !Settings.mtd) { return false; }
             if (text.Contains(".mtp") && !Settings.mtp) { return false; }
             if (text.Contains(".ogg") && !Settings.ogg) { return false; }
             if (text.Contains(".ogg2d") && !Settings.ogg2d) { return false; }
             if (text.Contains(".oggsg") && !Settings.oggsg) { return false; }
+            if (text.Contains(".ot") && !Settings.ot) { return false; }
+            if (text.Contains(".otc") && !Settings.otc) { return false; }
             if (text.Contains(".pet") && !Settings.pet) { return false; }
             if (text.Contains(".pjd") && !Settings.pjd) { return false; }
+            if (text.Contains(".png") && !Settings.png) { return false; }
+            if (text.Contains(".properties") && !Settings.properties) { return false; }
             if (text.Contains(".psg") && !Settings.psg) { return false; }
+            if (text.Contains(".red") && !Settings.red) { return false; }
             if (text.Contains(".rs") && !Settings.rs) { return false; }
+            if (text.Contains(".rtf") && !Settings.rtf) { return false; }
+            if (text.Contains(".slt") && !Settings.slt) { return false; }
             if (text.Contains(".sm") && !Settings.sm) { return false; }
             if (text.Contains(".smd") && !Settings.smd) { return false; }
             if (text.Contains(".tdt") && !Settings.tdt) { return false; }
             if (text.Contains(".tgm") && !Settings.tgm) { return false; }
+            if (text.Contains(".tgr") && !Settings.tgr) { return false; }
             if (text.Contains(".tgt") && !Settings.tgt) { return false; }
             if (text.Contains(".tmd") && !Settings.tmd) { return false; }
             if (text.Contains(".trl") && !Settings.trl) { return false; }
@@ -113,6 +138,9 @@ namespace POEHudDumper
             if (text.Contains(".tst") && !Settings.tst) { return false; }
             if (text.Contains(".ttf") && !Settings.ttf) { return false; }
             if (text.Contains(".txt") && !Settings.txt) { return false; }
+            if (text.Contains(".ui") && !Settings.ui) { return false; }
+            if (text.Contains(".xls") && !Settings.xls) { return false; }
+            if (text.Contains(".xml") && !Settings.xml) { return false; }
 
             return true;
         }
--- src/POEHudDumper/DumperSettings.cs
+++ src/POEHudDumper/DumperSettings.cs
@@ -10,25 +10,41 @@ namespace POEHudDumper
         {
             DumpPreloadData = false;
             FileExtensions = new EmptyNode();
+            FileExtensionsPart1 = new EmptyNode();
+            FileExtensionsPart2 = new EmptyNode();
             a = new EmptyNode();
             b = new EmptyNode();
+            c = new EmptyNode();
             d = new EmptyNode();
             e = new EmptyNode();
             f = new EmptyNode();
             g = new EmptyNode();
+            i = new EmptyNode();
+            j = new EmptyNode();
             m = new EmptyNode();
             o = new EmptyNode();
             p = new EmptyNode();
             r = new EmptyNode();
             s = new EmptyNode();
             t = new EmptyNode();
+            u = new EmptyNode();
+            x = new EmptyNode();
 
+            act = true;
+            ais = true;
             amd = true;
             ao = true;
+            aoc = true;
             arm = true;
             ast = true;
+            atlas = true;
             bank = true;
+            bat = true;
+            cfg = true;
+            cht = true;
+            clt = true;
             dat = true;
+            dct = true;
             dds = true;
             ddt = true;
             dgr = true;
@@ -37,23 +53,39 @@ namespace POEHudDumper
             env = true;
             epk = true;
             et = true;
+            ffx = true;
             fmt = true;
+            fx = true;
             gft = true;
             gt = true;
+            idl = true;
+            idt = true;
+            jpeg = true;
+            jpg = true;
             mat = true;
+            mb = true;
+            mel = true;
             mtd = true;
             mtp = true;
             ogg = true;
             ogg2d = true;
             oggsg = true;
+            ot = true;
+            otc = true;
             pet = true;
             pjd = true;
+            png = true;
+            properties = true;
             psg = true;
+            red = true;
             rs = true;
+            rtf = true;
+            slt = true;
             sm = true;
             smd = true;
             tdt = true;
             tgm = true;
+            tgr = true;
             tgt = true;
             tmd = true;
             trl = true;
@@ -61,6 +93,9 @@ namespace POEHudDumper
             tst = true;
             ttf = true;
             txt = true;
+            ui = true;
+            xls = true;
+            xml = true;
         }
 
         [Menu("Dump Preload Data", 11)]
@@ -69,119 +104,195 @@ namespace POEHudDumper
 
         [Menu("File Extensions", 12)]
         public EmptyNode FileExtensions { get; set; }
+        [Menu("A => J", 412, 12)]
+        public EmptyNode FileExtensionsPart1 { get; set; }
+        [Menu("M => X", 413, 12)]
+        public EmptyNode FileExtensionsPart2 { get; set; }
 
-        [Menu("A", 21, 12)]
+        [Menu("A", 21, 412)]
         public EmptyNode a { get; set; }
-        [Menu("B", 22, 12)]
+        [Menu("B", 22, 412)]
         public EmptyNode b { get; set; }
-        [Menu("D", 23, 12)]
+        [Menu("C", 23, 412)]
+        public EmptyNode c { get; set; }
+        [Menu("D", 24, 412)]
         public EmptyNode d { get; set; }
-        [Menu("E", 24, 12)]
+        [Menu("E", 25, 412)]
         public EmptyNode e { get; set; }
-        [Menu("F", 25, 12)]
+        [Menu("F", 26, 412)]
         public EmptyNode f { get; set; }
-        [Menu("G", 26, 12)]
+        [Menu("G", 27, 412)]
         public EmptyNode g { get; set; }
-        [Menu("M", 27, 12)]
-        public EmptyNode m { get; set; }
-        [Menu("O", 28, 12)]
-        public EmptyNode o { get; set; }
-        [Menu("P", 29, 12)]
-        public EmptyNode p { get; set; }
-        [Menu("R", 210, 12)]
-        public EmptyNode r { get; set; }
-        [Menu("S", 211, 12)]
-        public EmptyNode s { get; set; }
-        [Menu("T", 212, 12)]
-        public EmptyNode t { get; set; }
+        [Menu("I", 28, 412)]
+        public EmptyNode i { get; set; }
+        [Menu("J", 29, 412)]
+        public EmptyNode j { get; set; }
 
-        [Menu(".amd", 3211, 21)]
+        [Menu("M", 210, 413)]
+        public EmptyNode m { get; set; }
+        [Menu("O", 211, 413)]
+        public EmptyNode o { get; set; }
+        [Menu("P", 212, 413)]
+        public EmptyNode p { get; set; }
+        [Menu("R", 213, 413)]
+        public EmptyNode r { get; set; }
+        [Menu("S", 214, 413)]
+        public EmptyNode s { get; set; }
+        [Menu("T", 215, 413)]
+        public EmptyNode t { get; set; }
+        [Menu("U", 216, 413)]
+        public EmptyNode u { get; set; }
+        [Menu("X", 217, 413)]
+        public EmptyNode x { get; set; }
+
+        [Menu(".act", 3211, 21)]
+        public ToggleNode act { get; set; }
+        [Menu(".ais", 3212, 21)]
+        public ToggleNode ais { get; set; }
+        [Menu(".amd", 3213, 21)]
         public ToggleNode amd { get; set; }
-        [Menu(".ao", 3212, 21)]
+        [Menu(".ao", 3214, 21)]
         public ToggleNode ao { get; set; }
-        [Menu(".arm", 3213, 21)]
+        [Menu(".aoc", 3215, 21)]
+        public ToggleNode aoc { get; set; }
+        [Menu(".arm", 3216, 21)]
         public ToggleNode arm { get; set; }
-        [Menu(".ast", 3214, 21)]
+        [Menu(".ast", 3217, 21)]
         public ToggleNode ast { get; set; }
+        [Menu(".atlas", 3218, 21)]
+        public ToggleNode atlas { get; set; }
 
         [Menu(".bank", 3221, 22)]
         public ToggleNode bank { get; set; }
+        [Menu(".bat", 3222, 22)]
+        public ToggleNode bat { get; set; }
 
-        [Menu(".dat", 3231, 23)]
+        [Menu(".cfg", 3231, 23)]
+        public ToggleNode cfg { get; set; }
+        [Menu(".cht", 3232, 23)]
+        public ToggleNode cht { get; set; }
+        [Menu(".clt", 3233, 23)]
+        public ToggleNode clt { get; set; }
+
+        [Menu(".dat", 3241, 24)]
         public ToggleNode dat { get; set; }
-        [Menu(".dds", 3232, 23)]
+        [Menu(".dct", 3242, 24)]
+        public ToggleNode dct { get; set; }
+        [Menu(".dds", 3243, 24)]
         public ToggleNode dds { get; set; }
-        [Menu(".ddt", 3234, 23)]
+        [Menu(".ddt", 3244, 24)]
         public ToggleNode ddt { get; set; }
-        [Menu(".dgr", 3235, 23)]
+        [Menu(".dgr", 3245, 24)]
         public ToggleNode dgr { get; set; }
-        [Menu(".dlp", 3236, 23)]
+        [Menu(".dlp", 3246, 24)]
         public ToggleNode dlp { get; set; }
 
-        [Menu(".ecf", 3241, 24)]
+        [Menu(".ecf", 3251, 25)]
         public ToggleNode ecf { get; set; }
-        [Menu(".env", 3242, 24)]
+        [Menu(".env", 3252, 25)]
         public ToggleNode env { get; set; }
-        [Menu(".epk", 3243, 24)]
+        [Menu(".epk", 3253, 25)]
         public ToggleNode epk { get; set; }
-        [Menu(".et", 3244, 24)]
+        [Menu(".et", 3254, 25)]
         public ToggleNode et { get; set; }
 
-        [Menu(".fmt", 3251, 25)]
+        [Menu(".ffx", 3261, 26)]
+        public ToggleNode ffx { get; set; }
+        [Menu(".fmt", 3262, 26)]
         public ToggleNode fmt { get; set; }
+        [Menu(".fx", 3263, 26)]
+        public ToggleNode fx { get; set; }
 
-        [Menu(".gft", 3261, 26)]
+        [Menu(".gft", 3271, 27)]
         public ToggleNode gft { get; set; }
-        [Menu(".gt", 3262, 26)]
+        [Menu(".gt", 3272, 27)]
         public ToggleNode gt { get; set; }
 
-        [Menu(".mat", 3271, 27)]
+        [Menu(".idl", 3281, 28)]
+        public ToggleNode idl { get; set; }
+        [Menu(".idt", 3282, 28)]
+        public ToggleNode idt { get; set; }
+
+        [Menu(".jpeg", 3291, 29)]
+        public ToggleNode jpeg { get; set; }
+        [Menu(".jpg", 3292, 29)]
+        public ToggleNode jpg { get; set; }
+
+        [Menu(".mat", 32101, 210)]
         public ToggleNode mat { get; set; }
-        [Menu(".mtd", 3272, 27)]
+        [Menu(".mb", 32102, 210)]
+        public ToggleNode mb { get; set; }
+        [Menu(".mel", 32103, 210)]
+        public ToggleNode mel { get; set; }
+        [Menu(".mtd", 32104, 210)]
         public ToggleNode mtd { get; set; }
-        [Menu(".mtp", 3273, 27)]
+        [Menu(".mtp", 32105, 210)]
         public ToggleNode mtp { get; set; }
 
-        [Menu(".ogg", 3281, 28)]
+        [Menu(".ogg", 32111, 211)]
         public ToggleNode ogg { get; set; }
-        [Menu(".ogg2d", 3282, 28)]
+        [Menu(".ogg2d", 32112, 211)]
         public ToggleNode ogg2d { get; set; }
-        [Menu(".oggsg", 3283, 28)]
+        [Menu(".oggsg", 32113, 211)]
         public ToggleNode oggsg { get; set; }
+        [Menu(".ot", 32114, 211)]
+        public ToggleNode ot { get; set; }
+        [Menu(".otc", 32115, 211)]
+        public ToggleNode otc { get; set; }
 
-        [Menu(".pet", 3291, 29)]
+        [Menu(".pet", 32121, 212)]
         public ToggleNode pet { get; set; }
-        [Menu(".pjd", 3292, 29)]
+        [Menu(".pjd", 32122, 212)]
         public ToggleNode pjd { get; set; }
-        [Menu(".psg", 923, 29)]
+        [Menu(".png", 32123, 212)]
+        public ToggleNode png { get; set; }
+        [Menu(".properties", 32124, 212)]
+        public ToggleNode properties { get; set; }
+        [Menu(".psg", 32125, 212)]
         public ToggleNode psg { get; set; }
 
-        [Menu(".rs", 32101, 210)]
+        [Menu(".red", 32131, 213)]
+        public ToggleNode red { get; set; }
+        [Menu(".rs", 32132, 213)]
         public ToggleNode rs { get; set; }
+        [Menu(".rtf", 32133, 213)]
+        public ToggleNode rtf { get; set; }
 
-        [Menu(".sm", 32111, 211)]
+        [Menu(".slt", 32141, 214)]
+        public ToggleNode slt { get; set; }
+        [Menu(".sm", 32142, 214)]
         public ToggleNode sm { get; set; }
-        [Menu(".smd", 32112, 211)]
+        [Menu(".smd", 32143, 214)]
         public ToggleNode smd { get; set; }
 
-        [Menu(".tdt", 32121, 212)]
+        [Menu(".tdt", 32151, 215)]
         public ToggleNode tdt { get; set; }
-        [Menu(".tgm", 32122, 212)]
+        [Menu(".tgm", 32152, 215)]
         public ToggleNode tgm { get; set; }
-        [Menu(".tgt", 32123, 212)]
+        [Menu(".tgr", 32153, 215)]
+        public ToggleNode tgr { get; set; }
+        [Menu(".tgt", 32154, 215)]
         public ToggleNode tgt { get; set; }
-        [Menu(".tmd", 32124, 212)]
+        [Menu(".tmd", 32155, 215)]
         public ToggleNode tmd { get; set; }
-        [Menu(".trl", 32125, 212)]
+        [Menu(".trl", 32156, 215)]
         public ToggleNode trl { get; set; }
-        [Menu(".tsi", 32126, 212)]
+        [Menu(".tsi", 32157, 215)]
         public ToggleNode tsi { get; set; }
-        [Menu(".tst", 32127, 212)]
+        [Menu(".tst", 32158, 215)]
         public ToggleNode tst { get; set; }
-        [Menu(".ttf", 32128, 212)]
+        [Menu(".ttf", 32159, 215)]
         public ToggleNode ttf { get; set; }
-        [Menu(".txt", 32129, 212)]
+        [Menu(".txt", 321510, 215)]
         public ToggleNode txt { get; set; }
+
+        [Menu(".ui", 32161, 216)]
+        public ToggleNode ui { get; set; }
+
+        [Menu(".xls", 32171, 217)]
+        public ToggleNode xls { get; set; }
+        [Menu(".xml", 32172, 217)]
+        public ToggleNode xml { get; set; }
 
     }
 }
